de273ad634ff18c81a8c2a8c7660cb649d307f52
jesus andres zini <Jesus.Zini@hstpathways.com>
2024-02-08 10:53:58 -0300

    add cuenta-cliente

--- develop/src/cuenta-clientes.js
+++ develop/src/cuenta-clientes.js
@@ -7,6 +7,23 @@ function cuentaClientes() {
     FACTURACION: 'FACTURACION',
   };
 
+  const MONTHS = [
+    'ENERO',
+    'FEBRERO',
+    'MARZO',
+    'ABRIL',
+    'MAYO',
+    'JUNIO',
+    'JULIO',
+    'AGOSTO',
+    'SEPTIEMBRE',
+    'OCTUBRE',
+    'NOVIEMBRE',
+    'DICIEMBRE',
+  ];
+
+  const STATE = ['Por cobrar', 'Vencido', 'Cancelado'];
+
   /**
    * INITIALIZATIONS
    */
@@ -21,163 +38,153 @@ function cuentaClientes() {
     // Limpiar la tabla CUENTA_CLIENTES
     limpiarTabla(cuentaClientesSheet);
 
-    // Obtener datos de la hoja FACTURACION
-    var dataRange = facturacionSheet.getDataRange();
-    var data = dataRange.getValues();
-
-    // Crear objeto para almacenar totales por cliente y mes
-    var totalsByClientAndMonth = {};
-
-    // Recorrer filas de la hoja FACTURACION
-    for (var i = 1; i < data.length; i++) {
-      var row = data[i];
-
-      // Extraer información relevante
-      var cliente = row[2];
-      var importe = parseFloat(
-        String(row[3]).replace('$', '').replace(',', '')
-      );
-      var fechaPago = new Date(row[6]);
-
-      // Obtener mes y año de la fecha de pago
-      var mes = fechaPago.getMonth() + 1; // Meses en JavaScript van de 0 a 11
-      var año = fechaPago.getFullYear();
-
-      // Inicializar objeto para el cliente si no existe
-      if (!totalsByClientAndMonth[cliente]) {
-        totalsByClientAndMonth[cliente] = {};
-      }
-
-      // Inicializar todos los meses con 0 si no existe
-      for (var j = 1; j <= 12; j++) {
-        if (!totalsByClientAndMonth[cliente][j]) {
-          totalsByClientAndMonth[cliente][j] = 0;
-        }
-      }
-
-      // Sumar importe al total correspondiente
-      totalsByClientAndMonth[cliente][mes] += importe;
+    for (var i = 0; i < STATE.length; i++) {
+      var estado = STATE[i];
+      var totales = obtenerTotalesPorEstado(estado);
+      escribirEstadoCuentaClientes(i, estado, totales);
+      agregarTotalesPorMes(totales);
     }
-
-    // Escribir totales en la hoja CUENTA_CLIENTES
-    escribirTotalesEnCuentaClientes(totalsByClientAndMonth);
   })();
 
   /**
    * FEATURES
    */
-  // Función para escribir los totales en la hoja CUENTA_CLIENTES
-  function escribirTotalesEnCuentaClientes(totalsByClientAndMonth) {
-    // Obtener las columnas de la hoja CUENTA_CLIENTES
-    var columnas = cuentaClientesSheet.getRange(1, 1, 1, 13).getValues()[0];
-
-    // Recorrer el objeto totalsByClientAndMonth
-    for (var cliente in totalsByClientAndMonth) {
-      if (totalsByClientAndMonth.hasOwnProperty(cliente)) {
-        // Encontrar la fila correspondiente al cliente
-        var filaCliente = encontrarFilaCliente(cliente);
-
-        // Recorrer los meses y escribir los totales en las columnas correspondientes
-        for (var mes in totalsByClientAndMonth[cliente]) {
-          if (totalsByClientAndMonth[cliente].hasOwnProperty(mes)) {
-            // Encontrar la columna correspondiente al mes
-            var columnaMes =
-              columnas.indexOf(obtenerNombreMes(Number(mes))) + 1;
-
-            // Escribir el total en la celda correspondiente
-            cuentaClientesSheet
-              .getRange(filaCliente, columnaMes)
-              .setValue(totalsByClientAndMonth[cliente][mes])
-              .setNumberFormat('$#,##0.00');
-          }
-        }
-      }
-    }
-
-    // Agregar una fila adicional con los totales
-    agregarFilaTotales();
-  }
-
-  // Función para encontrar la fila correspondiente a un cliente en la hoja CUENTA_CLIENTES
-  function encontrarFilaCliente(cliente) {
-    var lastRow = cuentaClientesSheet.getLastRow();
-    if (lastRow < 2) {
-      // Si la hoja está vacía, agregar una nueva fila con el nombre del cliente
-      cuentaClientesSheet.getRange(2, 1).setValue(cliente);
-      return 2;
-    } else {
-      // Si hay filas, buscar el cliente existente
-      var clientesColumna = cuentaClientesSheet
-        .getRange(2, 1, lastRow - 1, 1)
-        .getValues();
-      for (var i = 0; i < clientesColumna.length; i++) {
-        if (clientesColumna[i][0] === cliente) {
-          return i + 2; // Sumar 2 para ajustarse a la indexación de Google Sheets
-        }
-      }
-      // Si no se encuentra el cliente, agregar una nueva fila con el nombre del cliente
-      var nuevaFila = cuentaClientesSheet.getLastRow() + 1;
-      cuentaClientesSheet.getRange(nuevaFila, 1).setValue(cliente);
-      return nuevaFila;
-    }
-  }
-
-  // Función para agregar una fila de totales al final de la hoja CUENTA_CLIENTES
-  function agregarFilaTotales() {
-    var lastRow = cuentaClientesSheet.getLastRow();
-    var lastColumn = cuentaClientesSheet.getLastColumn();
-
-    // Agregar "TOTAL" en negrita en la columna "CLIENTES"
+  function escribirEstadoCuentaClientes(i, estado, totales) {
+    var filaActual = i === 0 ? 1 : cuentaClientesSheet.getLastRow() + 3;
+    // Escribir el estado en la primera fila
     cuentaClientesSheet
-      .getRange(lastRow + 1, 1)
+      .getRange(filaActual, 1)
+      .setValue(estado)
+      .setBackground('#FFF2CC');
+
+    // Escribir el encabezado de meses y establecer colores
+    escribirEncabezadoCuentaClientes(filaActual + 1);
+
+    // Escribir los datos de clientes y totales
+    var clientes = Object.keys(totales);
+    for (var j = 0; j < clientes.length; j++) {
+      var cliente = clientes[j];
+      var datosCliente = [cliente];
+      for (var k = 0; k < MONTHS.length; k++) {
+        var mes = MONTHS[k].toLowerCase();
+        datosCliente.push(
+          totales[cliente][mes] ? totales[cliente][mes].total : 0
+        );
+      }
+      cuentaClientesSheet
+        .getRange(filaActual + j + 2, 1, 1, datosCliente.length)
+        .setValues([datosCliente])
+        .setNumberFormat('$#,##0.00')
+        .setFontFamily('Inconsolata');
+      cuentaClientesSheet.getRange('A:A').setFontFamily('Arial');
+    }
+  }
+
+  function escribirEncabezadoCuentaClientes(filaInicial) {
+    // Escribir encabezado de meses
+    var rangeHeader = cuentaClientesSheet.getRange(
+      filaInicial,
+      1,
+      1,
+      MONTHS.length + 1
+    );
+    rangeHeader.setValues([['CLIENTES'].concat(MONTHS)]);
+
+    // Establecer color de fondo para el encabezado de cliente (azul claro)
+    rangeHeader.getCell(1, 1).setBackground('#c9daf8');
+
+    // Establecer color de fondo para los meses (verde claro)
+    rangeHeader.offset(0, 1, 1, MONTHS.length).setBackground('#d9ead3');
+  }
+
+  function obtenerTotalesPorEstado(estado) {
+    // Objeto para almacenar los totales por cliente y mes
+    var totales = {};
+
+    // Iterar sobre las filas de la hoja de facturación
+    var data = facturacionSheet.getDataRange().getValues();
+    for (var i = 1; i < data.length; i++) {
+      // Empezamos desde 1 para saltar el encabezado
+      var cliente = data[i][2];
+      var importe = parseFloat(
+        String(data[i][3]).replace('$', '').replace(',', '')
+      );
+      var fechaVencimiento = new Date(data[i][5]);
+      var fechaPago = new Date(data[i][6]);
+      var mes;
+      if (!isNaN(fechaPago.getTime())) {
+        mes = fechaPago;
+      } else {
+        mes = fechaVencimiento;
+      }
+      var mes = MONTHS[mes.getMonth()].toLowerCase();
+      var estadoFacturacion = data[i][7];
+
+      // Verificar si el estado coincide
+      if (estadoFacturacion === estado) {
+        // Inicializar el objeto de totales si es la primera vez que se encuentra el cliente
+        if (!totales[cliente]) {
+          totales[cliente] = {};
+          MONTHS.forEach(function (month) {
+            totales[cliente][month.toLowerCase()] = { total: 0 };
+          });
+        }
+        // Sumar el importe al total correspondiente al cliente y al mes
+        totales[cliente][mes].total += importe;
+      }
+    }
+    return totales;
+  }
+
+  function agregarTotalesPorMes(totales) {
+    var filaActual = cuentaClientesSheet.getLastRow();
+    var clientes = Object.keys(totales);
+    var totalPorMes = {};
+
+    // Calcular el total por mes
+    for (var j = 0; j < clientes.length; j++) {
+      var cliente = clientes[j];
+      for (var k = 0; k < MONTHS.length; k++) {
+        var mes = MONTHS[k].toLowerCase();
+        totalPorMes[mes] =
+          (totalPorMes[mes] || 0) +
+          (totales[cliente][mes] ? totales[cliente][mes].total : 0);
+      }
+    }
+
+    // Escribir fila 'TOTAL'
+    var filaTotal = filaActual + 1;
+    cuentaClientesSheet
+      .getRange(filaTotal, 1)
       .setValue('TOTAL')
       .setFontWeight('bold');
 
-    // Calcular y sumar los totales para cada mes
-    for (var col = 2; col <= lastColumn; col++) {
-      var sumaTotal = 0;
-
-      // Sumar los valores de cada fila para la columna actual
-      for (var row = 2; row <= lastRow; row++) {
-        sumaTotal += cuentaClientesSheet.getRange(row, col).getValue() || 0;
-      }
-
-      // Escribir la suma total en la fila "TOTAL"
-      cuentaClientesSheet
-        .getRange(lastRow + 1, col)
-        .setValue(sumaTotal)
-        .setNumberFormat('$#,##0.00');
+    // Escribir totales por mes
+    var datosTotal = [''];
+    for (var l = 0; l < MONTHS.length; l++) {
+      var mes = MONTHS[l].toLowerCase();
+      datosTotal.push(totalPorMes[mes] || 0);
     }
+    // Eliminar el primer elemento vacío del array
+    datosTotal.shift();
+
+    cuentaClientesSheet
+      .getRange(filaTotal, 2, 1, datosTotal.length)
+      .setValues([datosTotal])
+      .setFontWeight('bold')
+      .setFontFamily('Inconsolata');
   }
 
   /**
    * UTILS
    */
-  // Función para obtener el nombre del mes a partir de su número (1-12)
-  function obtenerNombreMes(numeroMes) {
-    var meses = [
-      'ENERO',
-      'FEBRERO',
-      'MARZO',
-      'ABRIL',
-      'MAYO',
-      'JUNIO',
-      'JULIO',
-      'AGOSTO',
-      'SEPTIEMBRE',
-      'OCTUBRE',
-      'NOVIEMBRE',
-      'DICIEMBRE',
-    ];
-    return meses[numeroMes - 1];
-  }
-
   // Función para limpiar la tabla, manteniendo la fila de títulos
   function limpiarTabla(sheet) {
     var lastRow = sheet.getLastRow();
-    if (lastRow > 1) {
-      // Borrar todas las filas excepto la primera
-      sheet.deleteRows(2, lastRow - 1);
+    var lastColumn = sheet.getLastColumn();
+    if (lastRow > 0 && lastColumn > 0) {
+      // Borrar todas las filas y columnas
+      sheet.clear();
     }
   }
 }
